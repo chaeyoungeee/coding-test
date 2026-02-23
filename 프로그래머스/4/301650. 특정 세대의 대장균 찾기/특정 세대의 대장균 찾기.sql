45845826140abc9dca79eb61b43a989c69489f40
Chaeyoung Lim <102286483+chaeyoungeee@users.noreply.github.com>
2026-02-23 15:55:45 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -0,0 +1,7 @@
+select ID
+from ECOLI_DATA
+where PARENT_ID in (select ID
+                    from ECOLI_DATA
+                    where PARENT_ID in (select ID
+                                        from ECOLI_DATA
+                                        where PARENT_ID is null))
