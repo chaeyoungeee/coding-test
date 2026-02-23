2a17f66b05b694cbd0afb792bb9f0031e2ebbe53
Chaeyoung Lim <102286483+chaeyoungeee@users.noreply.github.com>
2026-02-23 12:57:19 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,5 @@
+select TITLE, b.BOARD_ID, REPLY_ID, r.WRITER_ID, r.CONTENTS, DATE_FORMAT(r.CREATED_DATE,'%Y-%m-%d')
+from USED_GOODS_BOARD b
+join USED_GOODS_REPLY r on b.BOARD_ID = r.BOARD_ID
+where year(b.CREATED_DATE) = '2022' and month(b.CREATED_DATE) = '10'
+order by r.CREATED_DATE, TITLE
