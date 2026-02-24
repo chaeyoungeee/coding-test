3c4b69751f65e3e240f0539a0157d4ab6e58105f
Chaeyoung Lim <102286483+chaeyoungeee@users.noreply.github.com>
2026-02-25 00:58:00 +0900

    [level 3] Title: 조건에 맞는 사용자와 총 거래금액 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164668. 조건에 맞는 사용자와 총 거래금액 조회하기/조건에 맞는 사용자와 총 거래금액 조회하기.sql
@@ -0,0 +1,7 @@
+select u.USER_ID, u.NICKNAME, sum(PRICE) TOTAL_SALES
+from USED_GOODS_BOARD b
+join USED_GOODS_USER u on b.WRITER_ID = u.USER_ID
+where STATUS = 'DONE'
+group by u.USER_ID, u.NICKNAME
+having sum(PRICE) >= 700000
+order by TOTAL_SALES
