a8da5cd790d58d25bf9fab618400d111a257c5ad
Chaeyoung Lim <102286483+chaeyoungeee@users.noreply.github.com>
2026-02-25 09:32:14 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -0,0 +1,6 @@
+select i.ANIMAL_ID, i.ANIMAL_TYPE, i.NAME
+from ANIMAL_INS i
+join ANIMAL_OUTS o on i.ANIMAL_ID = o.ANIMAL_ID
+where SEX_UPON_INTAKE like 'Intact%' 
+    and (SEX_UPON_OUTCOME like 'Neutered%' or SEX_UPON_OUTCOME like 'Spayed%')
+order by i.ANIMAL_ID
