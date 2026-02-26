472975ed32e01318b51c2b44b08cc18bafdbfe0b
Chaeyoung Lim <102286483+chaeyoungeee@users.noreply.github.com>
2026-02-26 21:41:05 +0900

    [level 2] Title: 가장 큰 수, Time: 185.58 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,28 +1,20 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 class Solution {
     public String solution(int[] numbers) {
+        int s = Arrays.stream(numbers).sum();
+        if (s == 0) return "0";
+        
         StringBuilder sb = new StringBuilder();
         
-        boolean allZero = true;
-        for (int number : numbers) {
-            if (number != 0) {
-                allZero = false;
-                break;
-            }
-        }
-        
-        if (allZero) {
-            return "0";
-        }
-        
-        Arrays.stream(numbers)
-            .boxed()
+        Arrays.stream(numbers).boxed()
             .map(String::valueOf)
-            .sorted((a, b) -> {
-                return (b+a).compareTo(a+b);
-            })
-            .forEach(s -> sb.append(s));
+            .sorted((a, b) -> (b+a).compareTo(a+b))
+            .forEach(n -> {
+                sb.append(n);
+            });
+        
         
         return sb.toString();
     }
