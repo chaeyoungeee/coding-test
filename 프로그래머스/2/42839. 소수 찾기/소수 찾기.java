5948ce498ae5bb59833fa669c889341c75d0ab6d
Chaeyoung Lim <102286483+chaeyoungeee@users.noreply.github.com>
2026-02-28 11:36:33 +0900

    [level 2] Title: 소수 찾기, Time: 27.87 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,25 +1,28 @@
 import java.util.*;
 
 class Solution {
-    static boolean isPrime(int n) {
-        if (n <= 1) return false;
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) return false;
-        }
-        return true;
-    }
-    
     static int n;
     static String number;
     static Set<Integer> set = new HashSet<>();
+    
+    static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        
+        for (int i = 2; i <= (int) Math.sqrt(n); i+=1)
+            if (n % i == 0) return false;
+        
+        return true;
+    }
     
     static void perm(int r, int depth, String result, boolean[] visited) {
         if (r == depth) {
             set.add(Integer.parseInt(result));
             return;
         }
+        
         for (int i = 0; i < n; i++) {
             if (depth == 0 && number.charAt(i) == '0') continue;
+            
             if (!visited[i]) {
                 visited[i] = true;
                 perm(r, depth+1, result + number.charAt(i), visited);
@@ -32,15 +35,10 @@ class Solution {
         n = numbers.length();
         number = numbers;
         
-        for (int i = 1; i < n+1; i++) {
-            perm(i, 0, "", new boolean[n]);   
-        }
+        for (int i = 1; i < n+1; i++) perm(i, 0, "", new boolean[n]);   
         
         int answer = 0;
-        for (int s : set) {
-            boolean p = isPrime(s);
-            if (p) answer +=1;
-        }
+        for (int s : set) if (isPrime(s)) answer +=1;
         return answer;
     }
 }
