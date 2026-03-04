95b294939f3eaff4881bbc5b0b9a9f855d654cca
Chaeyoung Lim <102286483+chaeyoungeee@users.noreply.github.com>
2026-03-04 22:22:27 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -1,5 +1,15 @@
+# select i.ANIMAL_ID, i.NAME
+# from ANIMAL_INS i
+# join ANIMAL_OUTS o on i.ANIMAL_ID = o.ANIMAL_ID
+# order by o.DATETIME - i.DATETIME desc
+# limit 2
+
+with temp as (
+select i.ANIMAL_ID, row_number() over(order by o.DATETIME - i.DATETIME desc) r
+from ANIMAL_INS i
+join ANIMAL_OUTS o on i.ANIMAL_ID = o.ANIMAL_ID)
+
 select i.ANIMAL_ID, i.NAME
 from ANIMAL_INS i
-join ANIMAL_OUTS o on i.ANIMAL_ID = o.ANIMAL_ID
-order by o.DATETIME - i.DATETIME desc
-limit 2
+join temp t on t.ANIMAL_ID = i.ANIMAL_ID
+where t.r <= 2
