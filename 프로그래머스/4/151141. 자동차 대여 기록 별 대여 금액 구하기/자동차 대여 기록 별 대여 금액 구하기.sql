80452f797c1525758fe692217b80809a493c950b
Chaeyoung Lim <102286483+chaeyoungeee@users.noreply.github.com>
2026-03-06 18:48:07 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,11 +1,16 @@
-select HISTORY_ID, round(DAILY_FEE * (datediff(h.END_DATE, h.START_DATE) + 1) * ((100-ifnull(DISCOUNT_RATE, 0)) / 100)) FEE
-from CAR_RENTAL_COMPANY_CAR c
-join CAR_RENTAL_COMPANY_RENTAL_HISTORY h on c.CAR_ID = h.CAR_ID
-left join CAR_RENTAL_COMPANY_DISCOUNT_PLAN p on c.CAR_TYPE = p.CAR_TYPE and
-        case when datediff(h.END_DATE, h.START_DATE) + 1 >= 90 then '90일 이상'
-            when datediff(h.END_DATE, h.START_DATE) + 1 >= 30 then '30일 이상'
-            when datediff(h.END_DATE, h.START_DATE) + 1 >= 7 then '7일 이상'
-            else '' end
-        = p.DURATION_TYPE
+with rate as (select replace(DURATION_TYPE, '일 이상', '') t, DISCOUNT_RATE r
+             from CAR_RENTAL_COMPANY_DISCOUNT_PLAN 
+             where CAR_TYPE ='트럭')
+
+select HISTORY_ID, round(DAILY_FEE * (datediff(h.END_DATE, h.START_DATE) + 1) * ((100-ifnull(r.r, 0)) / 100)) FEE
+from CAR_RENTAL_COMPANY_RENTAL_HISTORY h
+join CAR_RENTAL_COMPANY_CAR c on h.CAR_ID = c.CAR_ID
+left join rate r on
+    case 
+        when datediff(h.END_DATE, h.START_DATE) + 1 >= 90 then '90'
+        when datediff(h.END_DATE, h.START_DATE) + 1 >= 30 then '30'
+        when datediff(h.END_DATE, h.START_DATE) + 1 >= 7 then '7'
+        else '' end 
+    = r.t
 where c.CAR_TYPE = '트럭'
-order by FEE desc, HISTORY_ID desc
+order by 2 desc, 1 desc
