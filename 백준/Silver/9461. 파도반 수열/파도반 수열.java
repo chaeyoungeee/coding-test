049cb3b17990175bc9c9cc77a4be798670b622e7
Chaeyoung Lim <102286483+chaeyoungeee@users.noreply.github.com>
2026-03-06 14:24:53 +0900

    [Silver III] Title: 파도반 수열, Time: 108 ms, Memory: 14096 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[] coin;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        long[] dp = new long[101];
+        dp[1] = 1; dp[2] = 1; dp[3] = 1; dp[4] = 2; dp[5] = 2;
+
+        for (int i = 6; i < 101; i++) {
+            dp[i] = dp[i-1] + dp[i-5];
+        }
+
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+            System.out.println(dp[n]);
+        }
+    }
+}
