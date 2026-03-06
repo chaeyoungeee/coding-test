0a965212bf736982de6abd347461dfb8c59fd199
Chaeyoung Lim <102286483+chaeyoungeee@users.noreply.github.com>
2026-03-06 15:11:32 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -1,7 +1,6 @@
 select t2.ITEM_ID, i2.ITEM_NAME, i2.RARITY
-from ITEM_TREE t1
-join ITEM_INFO i1 on t1.ITEM_ID = i1.ITEM_ID
-join ITEM_TREE t2 on t1.ITEM_ID = t2.PARENT_ITEM_ID
+from ITEM_INFO i1
+join ITEM_TREE t2 on i1.ITEM_ID = t2.PARENT_ITEM_ID
 join ITEM_INFO i2 on t2.ITEM_ID = i2.ITEM_ID
 where i1.RARITY = 'RARE'
 order by 1 desc
