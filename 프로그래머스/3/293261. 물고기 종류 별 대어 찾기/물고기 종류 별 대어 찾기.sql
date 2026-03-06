39bde94009693f4829f742bf76cba50bc6e00ec0
Chaeyoung Lim <102286483+chaeyoungeee@users.noreply.github.com>
2026-03-06 17:24:48 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -1,11 +1,10 @@
-with big as (
-    select FISH_TYPE, max(LENGTH) LENGTH
-    from FISH_INFO
-    group by FISH_TYPE
+with rnk as (select ID, FISH_TYPE, LENGTH,
+        rank() over(partition by FISH_TYPE order by LENGTH desc) r
+from FISH_INFO
 )
 
 select ID, FISH_NAME, LENGTH
-from FISH_INFO f
-join FISH_NAME_INFO n on f.FISH_TYPE = n.FISH_TYPE
-where (f.FISH_TYPE, f.LENGTH) in (select * from big)
-order by ID
+from rnk r
+join FISH_NAME_INFO n on r.FISH_TYPE = n.FISH_TYPE
+where r = 1
+order by 1
