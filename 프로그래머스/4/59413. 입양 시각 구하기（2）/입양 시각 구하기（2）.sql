54f1981b9a00509f68aa711a8fcf945bf516db78
Chaeyoung Lim <102286483+chaeyoungeee@users.noreply.github.com>
2026-03-07 11:44:36 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,6 +1,5 @@
 set @hour := -1;
-select (@hour := @hour + 1) HOUR, 
-    (select count(*) from ANIMAL_OUTS where @hour = hour(DATETIME)) COUNT
+
+select @hour := @hour + 1 HOUR, (select count(*) from ANIMAL_OUTS o where hour(o.DATETIME) = @hour) COUNT
 from ANIMAL_OUTS
 where @hour < 23
-order by @hour
