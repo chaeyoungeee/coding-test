91ec3b7b7917e7673b1c9622dff4860f49b6c8a4
Chaeyoung Lim <102286483+chaeyoungeee@users.noreply.github.com>
2026-03-07 13:35:46 +0900

    [level 2] Title: 타겟 넘버, Time: 107.24 ms, Memory: 183 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=java) 
 
 ### 성능 요약
 
-메모리: 90.3 MB, 시간: 5.85 ms
+메모리: 183 MB, 시간: 107.24 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 29일 13:45:28
+2026년 03월 07일 13:35:45
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,25 +1,30 @@
+import java.util.*;
+
 class Solution {
-    
-    int len;
-    int target;
-    int answer = 0;
-    int[] numbers;
-    
-    public void dfs(int i, int sum) {
-        if (i == len) {
-            if (sum == target) answer += 1;
-            return;
+
+    static int bfs(int[] numbers, int target) {
+        int cnt = 0;
+            
+        int n = numbers.length;
+        ArrayDeque<int[]> q = new ArrayDeque<>();
+        
+        q.add(new int[] {0, numbers[0]});
+        q.add(new int[] {0, -numbers[0]});
+        
+        while(!q.isEmpty()) {
+            int[] now = q.poll();
+            if (now[0] == n-1) {
+                if (now[1] == target) cnt++;
+                continue;
+            }
+            q.add(new int[] {now[0]+1, now[1]+numbers[now[0]+1]});
+            q.add(new int[] {now[0]+1, now[1]-numbers[now[0]+1]});
         }
-        dfs(i+1, sum+numbers[i]);
-        dfs(i+1, sum-numbers[i]);
+        
+        return cnt;
     }
     
     public int solution(int[] numbers, int target) {
-        len = numbers.length;
-        this.target = target;
-        this.numbers = numbers;
-        
-        dfs(0, 0);
-        return answer;
+        return bfs(numbers, target);
     }
 }
