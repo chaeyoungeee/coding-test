109bcdd84362c30b0a986300ade6235a881bc921
Chaeyoung Lim <102286483+chaeyoungeee@users.noreply.github.com>
2026-07-04 16:29:41 +0900

    [level 2] Title: 전화번호 목록, Time: 209.36 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -3,13 +3,8 @@ import java.util.Arrays;
 class Solution {
     public boolean solution(String[] phone_book) {
         Arrays.sort(phone_book);
-        
         for (int i = 1; i < phone_book.length; i++) {
-            String prev = phone_book[i-1];
-            String curr = phone_book[i];
-            if (curr.substring(0, Math.min(curr.length(), prev.length())).equals(prev)) {
-                return false;
-            }
+            if (phone_book[i].startsWith(phone_book[i-1])) return false;
         }
         return true;
     }
