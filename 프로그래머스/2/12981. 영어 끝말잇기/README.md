9c20e0e1b56835b448b37d15c896632a73dbf1e5
Chaeyoung Lim <102286483+chaeyoungeee@users.noreply.github.com>
2026-07-11 13:37:30 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.04 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/README.md
+++ 프로그래머스/2/12981. 영어 끝말잇기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 영어 끝말잇기 - 12981 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981) 
 
 ### 성능 요약
 
-메모리: 89 MB, 시간: 0.06 ms
+메모리: 74.8 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 06일 17:10:49
+2026년 07월 11일 13:37:26
 
 ### 문제 설명
 
--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,23 +1,19 @@
-import java.util.HashSet;
+import java.util.*;
 
 class Solution {
     public int[] solution(int n, String[] words) {
-        int[] answer = {0, 0};
-
-        HashSet<String> set = new HashSet<>();
-        set.add(words[0]);
+        int[] answer = new int[] {0, 0};
+        Set<String> s = new HashSet<>();
+        s.add(words[0]);
         
         for (int i = 1; i < words.length; i++) {
-            String prev = words[i-1];
-            if (prev.charAt(prev.length()-1) == words[i].charAt(0)
-                && !set.contains(words[i])) {
-                set.add(words[i]);
-                continue;
+            if (s.contains(words[i]) || 
+                words[i-1].charAt(words[i-1].length()-1) != words[i].charAt(0)) {
+                answer[0] = (i+1)%n == 0 ? n : (i+1)%n; // 번호
+                answer[1] = (i/n)+1; // 차례
+                break;
             }
-            answer[0] = (i + 1) % n;
-            answer[1] = (i + n) / n;
-            if (answer[0] == 0) answer[0] = n;
-            break;
+            s.add(words[i]);
         }
 
         return answer;
