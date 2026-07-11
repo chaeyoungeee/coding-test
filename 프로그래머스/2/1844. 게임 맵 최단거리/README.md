42ba658dc421c2efae3f5511368ebe9e2cb36c5d
Chaeyoung Lim <102286483+chaeyoungeee@users.noreply.github.com>
2026-07-11 14:10:14 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.31 ms, Memory: 57.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 55.2 MB, 시간: 9.86 ms
+메모리: 57.3 MB, 시간: 4.31 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 12일 23:57:17
+2026년 07월 11일 14:10:11
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,43 +1,29 @@
 import java.util.ArrayDeque;
-import java.util.Arrays;
 
 class Solution {
-    
-    static int n, m;
-    static int[][] visited;
-    static int[][] dirs = { {1, 0}, {-1, 0}, {0, 1}, {0, -1} };
-    
-    static void bfs(int[][] maps) {
-        visited[0][0] = 1;
-        ArrayDeque<int[]> que = new ArrayDeque<>();
-        que.add(new int[] {0, 0});
+    public int solution(int[][] maps) {
+        ArrayDeque<int []> dq = new ArrayDeque<>();
+        dq.add(new int[] {0, 0});
+        int n = maps.length, m = maps[0].length;
+        int[][] dir = {{0,1}, {0,-1}, {1,0}, {-1,0}};
+        int[][] cost = new int[n][m];
+        cost[0][0] = 1;
         
-        while(!que.isEmpty()) {
-            int[] c = que.poll();
-            for (int[] dir : dirs) {
-                int nx = c[0] + dir[0];
-                int ny = c[1] + dir[1];
+        while (!dq.isEmpty()) {
+            int[] curr = dq.poll();
+            for (int[] d : dir) {
+                int nx = curr[0] + d[0];
+                int ny = curr[1] + d[1];
                 
-                if (nx < 0 || nx >= n || ny < 0 || ny >= m || 
-                    visited[nx][ny] != 0 || maps[nx][ny] == 0) continue;
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+                if (maps[nx][ny] == 0) continue;
                 
-                que.add(new int[] {nx, ny});
-                visited[nx][ny] = visited[c[0]][c[1]] + 1;
-                
-                if (nx == (n-1) && ny == (m-1)) return;
+                maps[nx][ny] = 0;
+                cost[nx][ny] = cost[curr[0]][curr[1]] + 1;
+                dq.add(new int[] {nx, ny});
             }
         }
-    }
-    
-    public int solution(int[][] maps) {
-        int answer = 0;
         
-        n = maps.length;
-        m = maps[0].length;
-        visited = new int[n][m];
-        
-        bfs(maps);
-        
-        return visited[n-1][m-1] == 0 ? -1 : visited[n-1][m-1];
+        return cost[n-1][m-1] == 0 ? -1 : cost[n-1][m-1];
     }
 }
